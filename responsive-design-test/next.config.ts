3094c5d1c6f3d181abe8088ee7802b49feda212c
chaewon127 <osobi621@gmail.com>
2026-08-26 10:54:15 +0900

    실습1 세팅

---
+++ responsive-design-test/next.config.ts
@@ -0,0 +1,14 @@
+/* next.config.mjs */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
