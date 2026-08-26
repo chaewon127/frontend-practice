88b6658cb8dd1682e713ba0d810475ccb4833785
chaewon127 <osobi621@gmail.com>
2026-08-26 10:58:57 +0900

    product info-responsive design-practice

--- responsive-design-test/src/app/page.tsx
+++ responsive-design-test/src/app/page.tsx
@@ -5,12 +5,14 @@ export default async function Home() {
   const product = await response.json();
 
   return (
-    <div className="items-center">
+    <div className="items-center md:flex">
       <Image src={product.image} alt={product.title} width={200} height={200} />
       <div>
-        <h1 className="font-bold">{product.title}</h1>
-        <p className="text-sm text-gray-500">{product.description}</p>
-        <p className="font-bold">$ {product.price}</p>
+        <h1 className="font-bold md:text-2xl">{product.title}</h1>
+        <p className="text-sm text-gray-500 md:text-lg">
+          {product.description}
+        </p>
+        <p className="font-bold md:text-2xl">$ {product.price}</p>
       </div>
     </div>
   );
