040370515e99d51dc759af7d27dad4bc066c857e
chaewon127 <osobi621@gmail.com>
2026-08-26 18:14:38 +0900

    rename dark-mode-test

---
+++ dark-mode-test/src/app/layout.tsx
@@ -0,0 +1,32 @@
+// src/layout.tsx
+
+import { ThemeProvider } from "next-themes";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+          // attribute="class"로 설정하면 html 태그에 class 추가
+          attribute="class"
+          // 기본 테마는 system
+          defaultTheme="system"
+          // 시스템에 반응하도록 설정
+          enableSystem
+          // 테마 전환 시 CSS transition 애니메이션을 비활성화
+          disableTransitionOnChange
+          // html 태그에 color-scheme 속성을 추가하지 않도록 설정
+          // true인 경우 의도치 않은 전체 배경 색상 변경도 적용됨
+          enableColorScheme={false}
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ dark-mode-test/src/providers/ThemeProvider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { createContext, useContext, useState } from "react";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+interface ThemeContextType {
+  toggleDarkMode: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error("useTheme은 ThemeProvider 내부에서 사용해야 합니다.");
+  }
+
+  return context;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [isDark, setIsDark] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDark(!isDark);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ toggleDarkMode }}>
+      {/* 버튼 삭제 */}
+      <div className={isDark ? "dark" : ""}>{children}</div>
+    </ThemeContext.Provider>
+  );
+}
