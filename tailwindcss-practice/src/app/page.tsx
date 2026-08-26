49d22ed5121c060aaa8c5a781dea4e4dc0bb748d
chaewon127 <osobi621@gmail.com>
2026-08-26 17:17:06 +0900

    gradient icon with tailwindcss-practice

--- tailwindcss-practice/src/app/page.tsx
+++ tailwindcss-practice/src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <span />
+      {/* h-6: 높이 24px */}
+      {/* w-6: 너비 24px */}
+      {/* rounded-full: 둥글게 */}
+      {/* bg-linear-to-tr: 그라데이션 효과 - top, right 방향으로 그라데이션 효과 */}
+      {/* from-[#096cde]: 시작 색상 */}
+      {/* from-30%: 그라데이션 시작점의 위치 */}
+      {/* to-[#ddf1ff]: 끝 색상 */}
+      <span className="size-10 rounded-full bg-linear-to-tr from-[#096cde] from-30% to-[#ddf1ff]" />
     </div>
   );
 }
