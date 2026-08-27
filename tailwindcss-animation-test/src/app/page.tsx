bef8edbe1cd94041ebc85f2f5570ceba955d563d
chaewon127 <osobi621@gmail.com>
2026-08-27 16:27:29 +0900

    card flip animation-practice

--- tailwindcss-animation-test/src/app/page.tsx
+++ tailwindcss-animation-test/src/app/page.tsx
@@ -9,12 +9,24 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <div className="cursor-pointer">
-        <div className={`relative h-96 w-64`}>
+      <div
+        // 7. perspective-midrange 클래스로 원근감 추가하기
+        // 마치 카메라 같은 역할을 하므로 3d 피사체(transform-3d)보다 부모 요소에 추가하기
+        className="cursor-pointer perspective-midrange"
+        // 3. 클릭을 통해 isFilpped 상태 변화
+        onClick={() => setIsFlipped(!isFlipped)}
+      >
+        <div
+          // 4. 클릭 시 isFlipped 상태에 따라 rotate-y-180 클래스 추가/제거
+          // 5. transform-3d 클래스로 3d로 회전할 수 있게 추가하기
+          // 6. transition-transform duration-700 클래스로 0.7초 동안 회전하도록 추가하기
+          className={`relative h-96 w-64 transition-transform duration-700 transform-3d ${isFlipped ? "rotate-y-180" : ""}`}
+        >
           <div className="absolute flex h-full w-full items-center justify-center rounded-xl bg-white p-4 shadow-lg">
             <div>카드앞면</div>
           </div>
-          <div className="absolute flex h-full w-full rotate-y-180 items-center justify-center rounded-xl bg-blue-500 p-4 text-white shadow-lg">
+          {/* 2. backface-hidden으로 180도 뒤집힌 부분을 보이제 않게 숨김 */}
+          <div className="absolute flex h-full w-full rotate-y-180 items-center justify-center rounded-xl bg-blue-500 p-4 text-white shadow-lg backface-hidden">
             <div>카드뒷면</div>
           </div>
         </div>
