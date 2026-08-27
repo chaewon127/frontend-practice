d3a3015919a5634220d5371327569ce994a2c3cc
chaewon127 <osobi621@gmail.com>
2026-08-27 13:35:40 +0900

    practice3 setting

--- css-animation-test/src/app/page.tsx
+++ css-animation-test/src/app/page.tsx
@@ -1,28 +1,50 @@
-// 실습 2- 버튼 hover 시 배경색 변경 애니매이션 with transition
-"use client";
-
-import { useState } from "react";
+// 실습 3 - 무한 슬라이드
+const SLIDES = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"];
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <div className="container">
-      <button
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}
-        className="btn"
-      >
-        아이콘 버튼
-      </button>
-      <div className={`menu-container ${isOpen ? "active" : ""}`}>
-        <div className="menu-item">메뉴1</div>
-        <div className="menu-item">메뉴2</div>
-        <div className="menu-item">메뉴3</div>
+    <div>
+      <div className="slideWrap">
+        <ul className="imgSlide original">
+          {SLIDES.map((slide, index) => (
+            <li key={`original-${index}`}>{slide}</li>
+          ))}
+        </ul>
+        <ul className="imgSlide clone">
+          {SLIDES.map((slide, index) => (
+            <li key={`clone-${index}`}>{slide}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
 }
+
+// 실습 2- 버튼 hover 시 배경색 변경 애니매이션 with transition
+// "use client";
+
+// import { useState } from "react";
+
+// export default function Home() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   return (
+//     <div className="container">
+//       <button
+//         onMouseEnter={() => setIsOpen(true)}
+//         onMouseLeave={() => setIsOpen(false)}
+//         className="btn"
+//       >
+//         아이콘 버튼
+//       </button>
+//       <div className={`menu-container ${isOpen ? "active" : ""}`}>
+//         <div className="menu-item">메뉴1</div>
+//         <div className="menu-item">메뉴2</div>
+//         <div className="menu-item">메뉴3</div>
+//       </div>
+//     </div>
+//   );
+// }
 
 // 실습 1 - transition, hover 애니매이션
 // export default function Home() {
