511488f551175fbb14c6583e07a0f6821f5f2fd9
chaewon127 <osobi621@gmail.com>
2026-08-27 13:31:35 +0900

    practice2 setting

--- css-animation-test/src/app/page.tsx
+++ css-animation-test/src/app/page.tsx
@@ -1,11 +1,26 @@
+// 실습 2- 버튼 hover 시 배경색 변경 애니매이션 with transition
 export default function Home() {
   return (
-    <div className="card-container">
-      <div className="card">영화 카드1</div>
-      <div className="card">영화 카드2</div>
-      <div className="card">영화 카드3</div>
-      <div className="card">영화 카드4</div>
-      <div className="card">영화 카드5</div>
+    <div className="container">
+      <button className="btn">아이콘 버튼</button>
+      <div className="menu-container">
+        <div className="menu-item">메뉴1</div>
+        <div className="menu-item">메뉴2</div>
+        <div className="menu-item">메뉴3</div>
+      </div>
     </div>
   );
 }
+
+// 실습 1 - transition, hover 애니매이션
+// export default function Home() {
+//   return (
+//     <div className="card-container">
+//       <div className="card">영화 카드1</div>
+//       <div className="card">영화 카드2</div>
+//       <div className="card">영화 카드3</div>
+//       <div className="card">영화 카드4</div>
+//       <div className="card">영화 카드5</div>
+//     </div>
+//   );
+// }
