9b03e934669d5bdc2ebfd718294b590e9068725a
chaewon127 <osobi621@gmail.com>
2026-08-27 18:21:05 +0900

    practice3 setting

--- farmer-motion-test/src/app/page.tsx
+++ farmer-motion-test/src/app/page.tsx
@@ -1,17 +1,25 @@
 import * as motion from "motion/react-client";
 
-// 실습 2 - 왼쪽에서 슬라이드 인
 export default function Home() {
   return (
-    <motion.div
-      initial={{ x: -100, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
-    >
-      왼쪽에서 슬라이드 인!
-    </motion.div>
+    <button className="rounded-md border-none bg-blue-500 px-5 py-2.5 text-white">
+      마우스를 올려보세요!
+    </button>
   );
 }
+
+// 실습 2 - 왼쪽에서 슬라이드 인
+// export default function Home() {
+//   return (
+//     <motion.div
+//       initial={{ x: -100, opacity: 0 }}
+//       animate={{ x: 0, opacity: 1 }}
+//       transition={{ duration: 0.8, ease: "easeOut" }}
+//     >
+//       왼쪽에서 슬라이드 인!
+//     </motion.div>
+//   );
+// }
 
 // 실습 1 - 페이드인
 // export default function Home() {
