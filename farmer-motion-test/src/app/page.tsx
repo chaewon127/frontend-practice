1a235f7d0a290741822d06121a9a34a562661697
chaewon127 <osobi621@gmail.com>
2026-08-27 17:40:46 +0900

    fadein motion-practice

--- farmer-motion-test/src/app/page.tsx
+++ farmer-motion-test/src/app/page.tsx
@@ -1,3 +1,13 @@
+import * as motion from "motion/react-client";
+
 export default function Home() {
-  return <div>안녕하세요! 페이드인 효과입니다.</div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      안녕하세요! 페이드인 효과입니다.
+    </motion.div>
+  );
 }
