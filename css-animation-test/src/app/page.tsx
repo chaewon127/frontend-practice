3b03060081f1ca274c212c6964b537bc80e4f8bd
chaewon127 <osobi621@gmail.com>
2026-08-27 13:33:58 +0900

    menu button hover-bg change animation-practice

--- css-animation-test/src/app/page.tsx
+++ css-animation-test/src/app/page.tsx
@@ -1,9 +1,21 @@
 // 실습 2- 버튼 hover 시 배경색 변경 애니매이션 with transition
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="container">
-      <button className="btn">아이콘 버튼</button>
-      <div className="menu-container">
+      <button
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+        className="btn"
+      >
+        아이콘 버튼
+      </button>
+      <div className={`menu-container ${isOpen ? "active" : ""}`}>
         <div className="menu-item">메뉴1</div>
         <div className="menu-item">메뉴2</div>
         <div className="menu-item">메뉴3</div>
