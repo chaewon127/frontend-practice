a1e0c87822c0ddbee3bd8070a54c7d0759c86bfe
chaewon127 <osobi621@gmail.com>
2026-08-27 17:41:56 +0900

    practice2 setting

--- farmer-motion-test/src/app/page.tsx
+++ farmer-motion-test/src/app/page.tsx
@@ -1,13 +1,18 @@
 import * as motion from "motion/react-client";
 
 export default function Home() {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 1 }}
-    >
-      안녕하세요! 페이드인 효과입니다.
-    </motion.div>
-  );
+  return <div>왼쪽에서 슬라이드 인!</div>;
 }
+
+// 실습 1 - 페이드인
+// export default function Home() {
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0 }}
+//       animate={{ opacity: 1 }}
+//       transition={{ duration: 1 }}
+//     >
+//       안녕하세요! 페이드인 효과입니다.
+//     </motion.div>
+//   );
+// }
