ec54e462fee640d8f3fdcc385af507ae6977ad29
chaewon127 <osobi621@gmail.com>
2026-08-27 18:21:41 +0900

    hover motion-practice

--- farmer-motion-test/src/app/page.tsx
+++ farmer-motion-test/src/app/page.tsx
@@ -2,9 +2,13 @@ import * as motion from "motion/react-client";
 
 export default function Home() {
   return (
-    <button className="rounded-md border-none bg-blue-500 px-5 py-2.5 text-white">
+    <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      className="rounded-md border-none bg-blue-500 px-5 py-2.5 text-white"
+    >
       마우스를 올려보세요!
-    </button>
+    </motion.button>
   );
 }
 
