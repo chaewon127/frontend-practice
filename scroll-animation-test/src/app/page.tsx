9809e4237673e20bfe07afbfc353bc557fe9cc14
chaewon127 <osobi621@gmail.com>
2026-08-28 09:36:03 +0900

    useInView hook-practice

--- scroll-animation-test/src/app/page.tsx
+++ scroll-animation-test/src/app/page.tsx
@@ -17,15 +17,20 @@
 //   );
 // }
 
+//----------------------------------------------------
+
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import { useInView } from "motion/react";
 
 export default function Home() {
   const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 1 });
   const [count, setCount] = useState(0);
 
   useEffect(() => {
+    if (!isInView) return;
     const interval = setInterval(() => {
       setCount((prev) => {
         if (prev >= 100) {
@@ -36,13 +41,13 @@ export default function Home() {
       });
     }, 20);
     return () => clearInterval(interval);
-  }, []);
+  }, [isInView]);
 
   return (
     <div className="flex h-[400dvh] items-center justify-center">
       <div
         ref={ref}
-        className="flex h-48 w-48 items-center justify-center bg-green-200 text-4xl font-bold"
+        className="flex h-48 w-48 items-center justify-center bg-green-200 text-4xl font-bold text-gray-500"
       >
         {count}
       </div>
