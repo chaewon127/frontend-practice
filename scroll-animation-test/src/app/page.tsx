968a9124b643f012ea459e7284ef743b0f98f1b0
chaewon127 <osobi621@gmail.com>
2026-08-28 09:28:55 +0900

    practice1 setting

---
+++ scroll-animation-test/src/app/page.tsx
@@ -0,0 +1,9 @@
+import * as motion from "motion/react-client";
+
+export default function Home() {
+  return (
+    <div className="flex h-[400dvh] items-center justify-center">
+      <div className="h-[300px] w-48 bg-blue-200">큰 박스</div>
+    </div>
+  );
+}
