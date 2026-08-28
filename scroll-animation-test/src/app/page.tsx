00acaf656dac0edc19d17fdbb518a1108f4a90b4
chaewon127 <osobi621@gmail.com>
2026-08-28 09:30:56 +0900

    whileInView-amount-practice

--- scroll-animation-test/src/app/page.tsx
+++ scroll-animation-test/src/app/page.tsx
@@ -3,7 +3,15 @@ import * as motion from "motion/react-client";
 export default function Home() {
   return (
     <div className="flex h-[400dvh] items-center justify-center">
-      <div className="h-[300px] w-48 bg-blue-200">큰 박스</div>
+      <motion.div
+        className="h-[300px] w-48 bg-blue-200"
+        initial={{ opacity: 0, scale: 0.8 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ amount: 0.8, once: true }}
+      >
+        박스
+      </motion.div>
     </div>
   );
 }
