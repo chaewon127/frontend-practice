a816e9e37599171e89d5ba079600bae7586354b5
chaewon127 <osobi621@gmail.com>
2026-08-28 09:38:07 +0900

    useScroll-practice

--- scroll-animation-test/src/app/page.tsx
+++ scroll-animation-test/src/app/page.tsx
@@ -60,11 +60,19 @@
 
 "use client";
 import * as React from "react";
-import { motion } from "motion/react";
+import { useScroll, useTransform, motion } from "motion/react";
 
 const PAGE_COUNT = 5;
 
 function ScrollLinked() {
+  // 1. 현재 스크롤 위치 추적
+  const { scrollYProgress } = useScroll();
+  // 2. 현재 스크롤 위치를 기반으로 클립 경로 계산
+  const clipPath = useTransform(
+    scrollYProgress,
+    (scrollYProgress) => `circle(${scrollYProgress * 100}%)`,
+  );
+
   return (
     <div className="h-full w-full bg-gray-900">
       <div className="fixed inset-0">
@@ -77,11 +85,9 @@ function ScrollLinked() {
           // h-full w-full: 요소의 높이와 너비를 100%로 설정
           // bg-orange-500: 오렌지색 배경
           className="absolute top-1/2 left-1/2 flex h-full w-full -translate-x-1/2 -translate-y-1/2 items-center justify-center bg-orange-500"
-          style={
-            {
-              // 여기에 css 속성을 추가해보세요.
-            }
-          }
+          style={{
+            clipPath,
+          }}
         >
           <div className="text-center">
             <h1 className="flex flex-col gap-4 text-8xl font-bold text-blue-600">
@@ -95,7 +101,6 @@ function ScrollLinked() {
           </div>
         </motion.div>
       </div>
-      {/* 페이지 수만큼 빈 div 생성하여 긴 페이지 생성 */}
       {new Array(PAGE_COUNT).fill(null).map((_, index) => (
         <div className="h-screen w-screen" key={index} />
       ))}
