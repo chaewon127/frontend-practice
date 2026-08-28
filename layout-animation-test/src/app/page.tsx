b0ef53bfc382abac3d493b8917a3dc028fce0552
chaewon127 <osobi621@gmail.com>
2026-08-28 15:08:26 +0900

    toggle button-layout animation-practice

--- layout-animation-test/src/app/page.tsx
+++ layout-animation-test/src/app/page.tsx
@@ -1,22 +1,34 @@
 "use client";
 
 import * as motion from "motion/react-client";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function LayoutAnimation() {
   const [isOn, setIsOn] = useState(false);
+  const [buttonWidth, setButtonWidth] = useState(0);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    if (buttonRef.current) {
+      const width = buttonRef.current.offsetWidth;
+      console.log(width);
+      setButtonWidth(width);
+    }
+  }, []);
 
   const toggleSwitch = () => setIsOn(!isOn);
 
   return (
     <div className="flex h-screen items-center justify-center">
       <button
+        ref={buttonRef}
         className={`flex h-18 w-32 cursor-pointer rounded-full bg-purple-800 p-3`}
         onClick={toggleSwitch}
       >
         <motion.div
           className="h-12 w-12 rounded-full bg-purple-500"
-          animate={{ x: isOn ? 58 : 0 }}
+          // 72 = 원의 너비(48px)와 좌우 패딩(12px, 12px)의 합
+          animate={{ x: isOn ? buttonWidth - 72 : 0 }}
           transition={{
             type: "spring",
             duration: 0.2,
