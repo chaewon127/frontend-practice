c32f678dce9aed1068b682faa0b647c1cf18d854
chaewon127 <osobi621@gmail.com>
2026-09-01 16:20:29 +0900

    practice3 setting

--- jest-test/validate.js
+++ jest-test/validate.js
@@ -1,6 +1,15 @@
-// 공통으로 사용되는 유효성 검사 함수
-function validatePassword(password) {
-  return password.length >= 8;
+// 실습 2 - 테스트 맛보기_유효성 검사
+// // 공통으로 사용되는 유효성 검사 함수
+// function validatePassword(password) {
+//   return password.length >= 8;
+// }
+
+// module.exports = { validatePassword };
+
+// 실습 3 - 이메일 유효성 검사
+function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
 }
 
-module.exports = { validatePassword };
+module.exports = { validateEmail };
--- jest-test/validate.test.js
+++ jest-test/validate.test.js
@@ -1,9 +1,10 @@
-const { validatePassword } = require("./validate");
+// 실습 2 - 테스트 맛보기_유효성 검사
+// const { validatePassword } = require("./validate");
 
-test("비밀번호가 8자 이상인 경우 true가 반환되는지 확인", () => {
-  expect(validatePassword("password123")).toBe(true);
-});
+// test("비밀번호가 8자 이상인 경우 true가 반환되는지 확인", () => {
+//   expect(validatePassword("password123")).toBe(true);
+// });
 
-test("비밀번호가 8자 미만인 경우 false가 반환되는지 확인", () => {
-  expect(validatePassword("pass")).toBe(false);
-});
+// test("비밀번호가 8자 미만인 경우 false가 반환되는지 확인", () => {
+//   expect(validatePassword("pass")).toBe(false);
+// });
