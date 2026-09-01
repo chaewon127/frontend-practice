0a624a4a3218e3a7e5ea40874e6348e48c44bb19
chaewon127 <osobi621@gmail.com>
2026-09-01 16:22:46 +0900

    email validation-jest-practice

--- jest-test/validate.test.js
+++ jest-test/validate.test.js
@@ -8,3 +8,19 @@
 // test("비밀번호가 8자 미만인 경우 false가 반환되는지 확인", () => {
 //   expect(validatePassword("pass")).toBe(false);
 // });
+
+// 실습 3 - 이메일 유효성 검사
+const { validateEmail } = require("./validate");
+
+test("유효한 이메일 입력 시 true를 반환하는지 확인", () => {
+  expect(validateEmail("test@example.com")).toBeTruthy();
+});
+
+test("유효하지 않은 이메일 입력 시 false를 반환하는지 확인", () => {
+  expect(validateEmail("testexample.com")).toBeFalsy();
+});
+
+// not을 사용하여 true가 아님을 확인
+test("도메인 없이 이메일을 허용하지 않아야 함", () => {
+  expect(validateEmail("test@")).not.toBeTruthy();
+});
