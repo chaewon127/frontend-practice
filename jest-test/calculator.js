72d2db937af03e837f0f5d69864be328d0b89271
chaewon127 <osobi621@gmail.com>
2026-09-01 15:42:49 +0900

    price calculation 2-jest-practice

--- jest-test/calculator.js
+++ jest-test/calculator.js
@@ -2,12 +2,12 @@ function calculateTotal(price, quantity, discount = 0) {
   return price * quantity * (1 - discount);
 }
 
-// 1. 장바구니 페이지에서 다음과 같이 작성
-const cartTotal = calculateTotal(1000, 2, 0.1); // 1800원
-console.log(`장바구니 총 가격: ${cartTotal}원`);
+// // 1. 장바구니 페이지에서 다음과 같이 작성
+// const cartTotal = calculateTotal(1000, 2, 0.1); // 1800원
+// console.log(`장바구니 총 가격: ${cartTotal}원`);
 
-// 2. 결제 페이지에서 다음과 같이 작성
-const paymentTotal = calculateTotal(2000, 4); // NaN원
-console.log(`결제 총 가격: ${paymentTotal}원`);
+// // 2. 결제 페이지에서 다음과 같이 작성
+// const paymentTotal = calculateTotal(2000, 4); // NaN원
+// console.log(`결제 총 가격: ${paymentTotal}원`);
 
 module.exports = { calculateTotal };
