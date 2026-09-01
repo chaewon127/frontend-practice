af12c6c06d74f1af0ce7a39fa00506e365856f37
chaewon127 <osobi621@gmail.com>
2026-09-01 15:47:24 +0900

    validation-jest-practice

---
+++ jest-test/validate.test.js
@@ -0,0 +1,9 @@
+const { validatePassword } = require("./validate");
+
+test("비밀번호가 8자 이상인 경우 true가 반환되는지 확인", () => {
+  expect(validatePassword("password123")).toBe(true);
+});
+
+test("비밀번호가 8자 미만인 경우 false가 반환되는지 확인", () => {
+  expect(validatePassword("pass")).toBe(false);
+});
