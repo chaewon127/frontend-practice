65ec00cd48095fcc1682f7d625f02fbfe00db603
chaewon127 <osobi621@gmail.com>
2026-09-02 16:10:15 +0900

    practice 1-3 setting

--- jest-framework-test/math.js
+++ jest-framework-test/math.js
@@ -1,8 +1,10 @@
 // 실습 1-1 모킹 기초 연습 - jest.fn() 사용법
 // 실습 1-2 모킹 기초 연습 - mockReturnValue 사용법
+// 실습 1-3 모킹 기초 연습 - spyOn 사용법
 const calculator = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
 };
 
 module.exports = {
