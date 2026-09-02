937c8c30981f3db3dce86a0e5895880c96afcce1
chaewon127 <osobi621@gmail.com>
2026-09-02 10:08:19 +0900

    practice setting

---
+++ jest-mocking-test/apiClient.test.js
@@ -0,0 +1,18 @@
+const { fetchData } = require("./apiClient");
+
+describe("apiClient.js 테스트", () => {
+  test("API 호출 후 데이터 포맷이 올바르게 되는지 확인", async () => {
+    // Arrange
+    const url = "https://jsonplaceholder.typicode.com/users/1";
+
+    // Act
+    const result = await fetchData(url);
+
+    // Assert
+    expect(result).toEqual({
+      userId: 1,
+      formattedName: "LEANNE GRAHAM",
+      address: "Kulas Light Apt. 556 Gwenborough",
+    });
+  });
+});
