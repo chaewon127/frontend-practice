2590ad6b860d34fae1414f17dd3e27912beb781d
chaewon127 <osobi621@gmail.com>
2026-09-02 10:13:00 +0900

    add mockResolvedValue

--- jest-mocking-test/apiClient.test.js
+++ jest-mocking-test/apiClient.test.js
@@ -2,8 +2,19 @@ const { fetchData } = require("./apiClient");
 
 describe("apiClient.js 테스트", () => {
   test("API 호출 후 데이터 포맷이 올바르게 되는지 확인", async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true, // fetch가 성공적으로 호출되었음을 나타냄
+      json: jest.fn().mockResolvedValue({
+        id: 1,
+        name: "Leanne Graham",
+        address: {
+          street: "Kulas Light",
+          suite: "Apt. 556",
+          city: "Gwenborough",
+        },
+      }),
+    });
     // Arrange
-    global.fetch = jest.fn();
     const url = "https://jsonplaceholder.typicode.com/users/1";
 
     // Act
