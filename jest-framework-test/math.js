336082fcaaca6376a88ad388599a7e2d771f45a3
chaewon127 <osobi621@gmail.com>
2026-09-02 16:08:08 +0900

    practice 1-2 setting

--- jest-framework-test/math.js
+++ jest-framework-test/math.js
@@ -1,6 +1,8 @@
 // 실습 1-1 모킹 기초 연습 - jest.fn() 사용법
+// 실습 1-2 모킹 기초 연습 - mockReturnValue 사용법
 const calculator = {
   add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
 };
 
 module.exports = {
--- jest-framework-test/math.test.js
+++ jest-framework-test/math.test.js
@@ -11,3 +11,15 @@ test("calculator.add 함수 모킹 테스트", () => {
   // 3. 호출 인자를 검증하세요
   expect(calculator.add).toHaveBeenCalledWith(1, 2);
 });
+
+//-----------------------------------------------------
+
+// 실습 1-2 모킹 기초 연습 - mockReturnValue 사용법
+test("calculator.subtract 함수 모킹 테스트", () => {
+  // 여기에 코드 작성
+  // 1. calculator.subtract가 1을 반환하도록 jest.fn()으로 모킹하세요
+  // 2. 모킹된 함수를 호출하고, 호출 여부를 검증하세요.
+  //    -> subtract 함수에는 5와 3을 인자로 전달하세요.
+  // 3. calculator.subtract 함수가 1을 반환하는지 검증하세요.
+  //    -> 5 - 3은 2지만 모킹으로 인해 1이 반환됩니다.
+});
