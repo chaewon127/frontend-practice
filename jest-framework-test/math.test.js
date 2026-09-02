523146b29cf663e4e11aca9ae2244fe301fcc4fe
chaewon127 <osobi621@gmail.com>
2026-09-02 16:06:37 +0900

    jest.fn

--- jest-framework-test/math.test.js
+++ jest-framework-test/math.test.js
@@ -4,6 +4,10 @@ const { calculator } = require("./math");
 test("calculator.add 함수 모킹 테스트", () => {
   // 여기에 코드 작성
   // 1. calculator.add를 jest.fn()으로 모킹하세요
+  calculator.add = jest.fn();
   // 2. 모킹된 함수를 호출해보고, 호출되었는지 검증하세요
+  calculator.add(1, 2);
+  expect(calculator.add).toHaveBeenCalled();
   // 3. 호출 인자를 검증하세요
+  expect(calculator.add).toHaveBeenCalledWith(1, 2);
 });
