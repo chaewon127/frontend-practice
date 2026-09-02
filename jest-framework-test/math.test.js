221250fc630d7bc1bba14e54053eb4283bc527d4
chaewon127 <osobi621@gmail.com>
2026-09-02 16:10:40 +0900

    spyOn

--- jest-framework-test/math.test.js
+++ jest-framework-test/math.test.js
@@ -34,7 +34,11 @@ test("calculator.subtract 함수 모킹 테스트", () => {
 test("calculator.multiply 함수 모킹 테스트", () => {
   // 여기에 코드 작성
   // 1. calculator.multiply를 spyOn으로 모킹하세요
+  const multiplySpy = jest.spyOn(calculator, "multiply");
   // 2. 모킹된 함수가 호출되었는지 검증하세요
   //    -> 이때 첫 번째 인자는 2, 두 번째 인자는 3을 넣어주세요.
+  multiplySpy(2, 3);
+  expect(multiplySpy).toHaveBeenCalled();
   // 3. 호출 결과가 올바르게 계산되는지 검증하세요 (2 * 3 = 6)
+  expect(multiplySpy(2, 3)).toBe(6);
 });
