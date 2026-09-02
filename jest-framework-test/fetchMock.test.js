2557cceae36c414cd5cb3c26c9fd07aa54ad3192
chaewon127 <osobi621@gmail.com>
2026-09-02 16:26:41 +0900

    resolves & rejects Matcher

--- jest-framework-test/fetchMock.test.js
+++ jest-framework-test/fetchMock.test.js
@@ -1,5 +1,6 @@
+const { fetchUserData } = require("./fetchMock");
+
 // 실습 1-4 모킹 기초 연습 - fetch 모킹 & mockResolvedValue 사용법
-// const { fetchUserData } = require("./fetchMock");
 
 // // 테스트 코드 작성
 // test("fetchUserData 함수 테스트", async () => {
@@ -19,8 +20,6 @@
 //-----------------------------------------------------
 
 // 실습 3-1 비동기 테스트 연습 - async/await 방식 검증
-// const { fetchUserData } = require("./fetchMock");
-
 // test("사용자가 데이터를 성공적으로 가져오는 경우 모킹", async () => {
 //   const mockUserData = { id: 1, name: "철수", email: "abc@naver.com" };
 //   // 1. fetch API 모킹하기
@@ -53,8 +52,6 @@
 //-----------------------------------------------------
 
 // 실습 3-2 비동기 테스트 연습 - Promise 방식 검증
-// const { fetchUserData } = require("./fetchMock");
-
 // test("사용자 데이터를 성공적으로 가져오는 경우 - Promise 방식", () => {
 //   const mockUserData = { id: 1, name: "철수", email: "abc@naver.com" };
 
@@ -86,9 +83,7 @@
 //-----------------------------------------------------
 
 // 실습 3-3 비동기 테스트 연습 - resolves, rejects Matcher 사용 검증
-const { fetchUserData } = require("./fetchMock");
-
-test("사용자 데이터를 성공적으로 가져오는 경우 - Promise 방식", () => {
+test("사용자 데이터를 성공적으로 가져오는 경우 - resolves 방식", () => {
   const mockUserData = { id: 1, name: "철수", email: "abc@naver.com" };
 
   // 1. fetch API 모킹하기
@@ -97,21 +92,14 @@ test("사용자 데이터를 성공적으로 가져오는 경우 - Promise 방�
     json: jest.fn().mockResolvedValue(mockUserData),
   });
 
-  // 2. Promise 방식으로 테스트
-  return fetchUserData(1).then((userData) => {
-    expect(userData).toEqual(mockUserData);
-  });
+  // 2. resolves 방식으로 테스트
+  return expect(fetchUserData(1)).resolves.toEqual(mockUserData);
 });
 
-test("네트워크 오류 발생 시 에러 처리 - Promise 방식", () => {
+test("네트워크 오류 발생 시 에러 처리 - rejects 방식", () => {
   // 1. fetch가 거부된 Promise를 반환하도록 모킹
   global.fetch = jest.fn().mockRejectedValue(new Error("Network Error"));
 
-  // 2. 최소 1개의 assertion이 필요
-  expect.assertions(1);
-
-  // 3. Promise 방식으로 에러 처리 테스트
-  return fetchUserData(1).catch((error) => {
-    expect(error.message).toBe("Network Error");
-  });
+  // 2. rejects 방식으로 에러 처리 테스트
+  return expect(fetchUserData(1)).rejects.toThrow("Network Error");
 });
