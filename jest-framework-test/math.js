33164d9fc77d2ed29714b1627c0a900d05caf601
chaewon127 <osobi621@gmail.com>
2026-09-02 16:05:33 +0900

    practice1-1 setting

---
+++ jest-framework-test/math.js
@@ -0,0 +1,8 @@
+// 실습 1-1 모킹 기초 연습 - jest.fn() 사용법
+const calculator = {
+  add: (a, b) => a + b,
+};
+
+module.exports = {
+  calculator,
+};
---
+++ jest-framework-test/math.test.js
@@ -0,0 +1,9 @@
+// 실습 1-1 모킹 기초 연습 - jest.fn() 사용법
+const { calculator } = require("./math");
+
+test("calculator.add 함수 모킹 테스트", () => {
+  // 여기에 코드 작성
+  // 1. calculator.add를 jest.fn()으로 모킹하세요
+  // 2. 모킹된 함수를 호출해보고, 호출되었는지 검증하세요
+  // 3. 호출 인자를 검증하세요
+});
