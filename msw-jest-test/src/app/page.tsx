beeade8e2325468192bda3c8eaba8000f4b8b120
chaewon127 <osobi621@gmail.com>
2026-09-04 18:26:46 +0900

    msw jest practice setting

---
+++ msw-jest-test/src/app/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+export default function Home() {
+  const [data, setData] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:4000/posts");
+      if (!response.ok) {
+        throw new Error("에러 발생");
+      }
+      const posts = await response.json();
+      setData(posts);
+    };
+    fetchData();
+  }, []);
+  return (
+    <ul>
+      {data.map((item) => (
+        <li key={item.id} className="border p-4">
+          <h3 className="font-bold">
+            {item.id}: {item.title}
+          </h3>
+          <p>{item.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
