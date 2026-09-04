f9d9764392497a5364e2808109472579f0b5bffd
chaewon127 <osobi621@gmail.com>
2026-09-04 18:45:08 +0900

    articel list data test-practice

--- msw-jest-test/src/app/page.test.ts
+++ msw-jest-test/src/app/page.test.ts
@@ -1,14 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import PostListPage from "./page";
+import Home from "@/app/page";
 
 describe("게시글 목록 페이지", () => {
   test("게시글 목록을 불러와 화면에 렌더링한다", async () => {
-    render(<PostListPage />);
+    render(
+      <Home />
+    );
+    const postListItems = await screen.findAllByRole("listitem");
 
-    // TODO: listitem 역할을 가진 요소가 2개인지 확인해보세요.
-
-    // TODO: "1: 첫 번째 게시글" 텍스트가 화면에 있는지 확인해보세요.
-
-    // TODO: "2: 두 번째 게시글" 텍스트가 화면에 있는지 확인해보세요.
+    expect(postListItems).toHaveLength(2);
+    expect(screen.getByText("1: 첫 번째 게시글")).toBeInTheDocument();
+    expect(screen.getByText("2: 두 번째 게시글")).toBeInTheDocument();
   });
 });
