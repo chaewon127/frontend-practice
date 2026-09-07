d758eaeaa143a2e09ffe4bd9b376bb8e883d1225
chaewon127 <osobi621@gmail.com>
2026-09-07 11:33:38 +0900

    storybook 분석-TailwinCSS 적용

--- snapshot-storybook-test/.storybook/preview.tsx
+++ snapshot-storybook-test/.storybook/preview.tsx
@@ -1,11 +1,27 @@
-import type { Preview } from '@storybook/nextjs-vite'
+import type { Preview } from "@storybook/nextjs-vite";
+import "../src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        // background 혹은 color로 끝나는 단어는 컬러 피커(Color Picker) UI로 자동 연결
+        color: /(background|color)$/i,
+        // date로 끝나는 단어는 날짜 선택기 UI로 자동 연결
+        date: /Date$/i,
+      },
+    },
+    // 전체 스토리에 배경 설정
+    backgrounds: {
+      options: {
+        red: {
+          name: "red",
+          value: "red",
+        },
+        blue: {
+          name: "blue",
+          value: "blue",
+        },
       },
     },
 
@@ -13,8 +29,8 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo'
-    }
+      test: "todo",
+    },
   },
 };
 
