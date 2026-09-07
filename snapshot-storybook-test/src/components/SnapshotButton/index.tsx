4ace25ffbe3cbd538f8e9f301a2403368ca20a27
chaewon127 <osobi621@gmail.com>
2026-09-07 11:13:48 +0900

    UI change snapshot update

--- snapshot-storybook-test/src/components/SnapshotButton/index.tsx
+++ snapshot-storybook-test/src/components/SnapshotButton/index.tsx
@@ -3,5 +3,7 @@ export default function SnapshotButton({
 }: {
   children: React.ReactNode;
 }) {
-  return <button>{children}</button>;
+  return <button className="bg-red-500">{children}</button>;
 }
+
+// UI 변경이 의도된 것이라면 npx jest --updateSnapshot
