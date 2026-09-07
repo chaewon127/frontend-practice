fbd3ed2857c7bd91f55e9ca7a98228985fb8a1a1
chaewon127 <osobi621@gmail.com>
2026-09-07 11:18:40 +0900

    SnapshotBUtton UI

--- snapshot-storybook-test/src/components/SnapshotButton/index.test.tsx
+++ snapshot-storybook-test/src/components/SnapshotButton/index.test.tsx
@@ -1,7 +1,23 @@
 import { render } from "@testing-library/react";
-import SnapshotButton from ".";
+import SnapshotButton from "./";
 
-test("Button 컴포넌트가 올바르게 렌더링됩니다", () => {
-  const { container } = render(<SnapshotButton>클릭하세요</SnapshotButton>);
-  expect(container).toMatchSnapshot();
+describe("Button 컴포넌트가 올바르게 렌더링됩니다.", () => {
+  test("기본 상태", () => {
+    const { container } = render(<SnapshotButton>클릭</SnapshotButton>);
+    expect(container).toMatchSnapshot();
+  });
+
+  test("비활성화 상태", () => {
+    const { container } = render(
+      <SnapshotButton disabled>클릭</SnapshotButton>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test("로딩 상태", () => {
+    const { container } = render(
+      <SnapshotButton isLoading>클릭</SnapshotButton>,
+    );
+    expect(container).toMatchSnapshot();
+  });
 });
--- snapshot-storybook-test/src/components/SnapshotButton/index.tsx
+++ snapshot-storybook-test/src/components/SnapshotButton/index.tsx
@@ -1,9 +1,21 @@
 export default function SnapshotButton({
   children,
+  disabled,
+  isLoading,
 }: {
   children: React.ReactNode;
+  disabled?: boolean;
+  isLoading?: boolean;
 }) {
-  return <button className="bg-red-500">{children}</button>;
+  return (
+    <button
+      className={`bg-blue-500 ${
+        disabled ? "opacity-50 cursor-not-allowed" : ""
+      } ${isLoading ? "animate-pulse" : ""}`}
+    >
+      {children}
+    </button>
+  );
 }
 
 // UI 변경이 의도된 것이라면 npx jest --updateSnapshot
